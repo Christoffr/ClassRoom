b71516795abebbc785e823a6be14633b51efe521
Christoffer <bendigsunne@hotmail.com>
2026-08-31 11:43:47 +0200

    Implemented Season method in Student

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,5 +1,5 @@
 ﻿using ClassRoomNet60;
 
-ClassRoom classRoom = new ClassRoom( "3D", new List<Student> { new Student("Christoffer", 2, 28) }, new DateTime(2026, 8, 26));
+ClassRoom classRoom = new ClassRoom( "3D", new List<Student> { new Student("Christoffer", 2, 28), new Student("Martin", 5, 6) }, new DateTime(2026, 8, 26));
 
 Console.WriteLine(classRoom);
