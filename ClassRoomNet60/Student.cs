b438b5088396d5950b5e21ad0853ba691c53bc48
Christoffer <bendigsunne@hotmail.com>
2026-08-31 11:14:38 +0200

    added student class

---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60;
+
+public class Student
+{
+    public string Name { get; private set; }
+    public int BirthMonth { get; private set; }
+    public int Birthday { get; private set; }
+
+    public Student(string name, int birthMonth, int birthday)
+    {
+        Name = name;
+        BirthMonth = birthMonth;
+        Birthday = birthday;
+    }
+}
