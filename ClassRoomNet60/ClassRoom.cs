7702b9e67cd00b17eeeebcd319472be46ea32bd5
Christoffer <bendigsunne@hotmail.com>
2026-08-31 11:17:55 +0200

    added classroom class

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60;
+
+public class ClassRoom
+{
+    public string ClassName { get; private set; }
+    public List<Student> StudentList { get; private set; }
+    public DateTime SemesterStart { get; private set; }
+
+    public ClassRoom(string className, List<Student> studentList, DateTime semesterStart)
+    {
+        ClassName = className;
+        StudentList = studentList;
+        SemesterStart = semesterStart;
+    }
+}
